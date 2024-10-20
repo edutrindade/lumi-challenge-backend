c75619415ff400437ab82c3c9f1e90a6f8684f12
Eduardo Trindade <eduardoepalmeiras@gmail.com>
2024-10-20 14:16:33 -0300

    feat: Nova função para converter data

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -8,4 +8,29 @@ const convertToDate = (dateStr: string): Date => {
    return new Date(year, month - 1, day)
 }
 
-export { formatDate, convertToDate }
+const convertMonth = (month: number): string => {
+   const months = ['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET', 'OUT', 'NOV', 'DEZ']
+
+   return months[month - 1]
+}
+
+const monthMap = (month: string): string => {
+   const months = {
+      JAN: '01',
+      FEV: '02',
+      MAR: '03',
+      ABR: '04',
+      MAI: '05',
+      JUN: '06',
+      JUL: '07',
+      AGO: '08',
+      SET: '09',
+      OUT: '10',
+      NOV: '11',
+      DEZ: '12',
+   }
+
+   return months[month]
+}
+
+export { formatDate, convertToDate, convertMonth, monthMap }
